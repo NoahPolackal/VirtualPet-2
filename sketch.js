921e3c704782b45ead851416efa690f2f433ddeb
Noah Polackal <71651076+NoahPolackal@users.noreply.github.com>
2020-12-20 08:36:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+var database ,dog,dog1,dog2
+var position
+var feed,add
+var foodobject
+var Feedtime
+var Lastfeed
+
+
+function preload()
+
+{
+  dogimg1 = loadImage("images/dogImg.png")
+  dogimg2 = loadImage("images/dogImg1.png")
+
+}
+
+function setup() {
+	createCanvas(1000, 500);
+  database = firebase.database();
+  console.log(database);
+ 
+  foodobject=new Food()
+  dog = createSprite(550,250,10,10);
+  dog.addImage(dogimg1)
+  dog.scale=0.2
+  
+ 
+
+var dogo = database.ref('Food');
+dogo.on("value", readPosition, showError);
+feed = createButton("FEED MIKE")
+feed.position(500,15)
+feed.mousePressed(FeedDog)
+add = createButton("ADD FOOD")
+add.position(400,15)
+add.mousePressed(AddFood)
+
+} 
+
+function draw(){
+ { background("gold");
+ foodobject.display()
+ }  
+drawSprites();
+}
+
+function readPosition(data){
+  position = data.val();
+  foodobject.updateFoodStock(position)
+  console.log(position.x);
+  
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
+
+function writePosition(N){
+  if(N>0){
+    N=N-1
+  }
+  else{
+    N=0
+  }
+  database.ref('/').set({
+    'Food': N
+  })
+
+}
+function AddFood(){
+position++
+database.ref('/').update({
+  Food:position
+}
+
+)
+}
+function FeedDog(){
+
+dog.addImage(dogimg2)
+foodobject.updateFoodStock(foodobject.getFoodStock()-1)
+ database.ref('/').update({
+   Food:foodobject.getFoodStock(),
+   FeedTime:hour ()
+ })
+}
